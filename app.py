469c116544510bb4f61f0eff41c7f22c6b049983
zsimjee <zaydsimjee@gmail.com>
2024-08-14 21:02:28 -0700

    update app.py to use new batching optimizations

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ from pydantic import BaseModel
 from typing import List, Union
 import detoxify
 import torch
-import json
+import os
 
 app = FastAPI()
 # Initialize the detoxify model once
-device = "cpu"
-model = detoxify.Detoxify('unbiased-small', device=torch.device(device))
+env = os.environ.get("env", "dev")
+torch_device = "cuda" if env == "prod" else "cpu"
+model = detoxify.Detoxify('unbiased-small', device=torch.device(torch_device))
 
 class InferenceData(BaseModel):
     name: str
     shape: List[int]
-    data: Union[List[str], List[float]]
+    data: List
     datatype: str
 
 class InputRequest(BaseModel):
@@ -26,32 +27,53 @@ class OutputResponse(BaseModel):
 
 @app.post("/validate", response_model=OutputResponse)
 async def check_toxicity(input_request: InputRequest):
-    text = None
     threshold = None
-    
     for inp in input_request.inputs:
         if inp.name == "text":
-            text = inp.data[0]
+            text_vals = inp.data
         elif inp.name == "threshold":
             threshold = float(inp.data[0])
-    
-    if text is None or threshold is None:
+
+    if text_vals is None or threshold is None:
         raise HTTPException(status_code=400, detail="Invalid input format")
-    results = model.predict(text)
-    pred_labels = [label for label, score in results.items() if score > threshold]
-    output_data = OutputResponse(
-        modelname="unbiased-small",
-        modelversion="1",
-        outputs=[
-            InferenceData(
-                name="result",
-                datatype="BYTES",
-                shape=[len(pred_labels)],
-                data=[pred_labels]
+
+    return ToxicLanguage.infer(text_vals, threshold)
+
+class ToxicLanguage:
+    model_name = "unbiased-small"
+    validation_method = "sentence"
+    device = torch.device(torch_device)
+    model = detoxify.Detoxify(model_name, device=device)
+    labels = [
+        "toxicity",
+        "severe_toxicity",
+        "obscene",
+        "threat",
+        "insult",
+        "identity_attack",
+        "sexual_explicit",
+    ]
+
+    def infer(text_vals, threshold) -> OutputResponse:
+        outputs = []
+        for idx, text in enumerate(text_vals):
+            results = ToxicLanguage.model.predict(text)
+            pred_labels = [
+                label for label, score in results.items() if score > threshold
+            ]
+            outputs.append(
+                InferenceData(
+                    name=f"result{idx}",
+                    datatype="BYTES",
+                    shape=[len(pred_labels)],
+                    data=[pred_labels],
+                )
             )
-        ]
-    )
-    
-    return output_data
+
+        output_data = OutputResponse(
+            modelname="unbiased-small", modelversion="1", outputs=outputs
+        )
+
+        return output_data
 # Run the app with uvicorn
 # Save this script as app.py and run with: uvicorn app:app --reload
